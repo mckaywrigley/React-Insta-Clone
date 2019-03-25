032429f7cc6a4e667553b3edcb788e6ad4b22d95
Mckay <mckaywrigley@gmail.com>
2019-03-25 10:40:35 -0700

    imports app.js work

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PostsPage from './components/PostContainer/PostsPage';
+import Authenticate from './components/authentication/Authentication';
+import Login from './components/LoginPage/Login';
+
+const ComponenentFromAuthenticate = Authenticate(PostsPage)(Login);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ComponenentFromAuthenticate />
     );
   }
 }
